91c385298fdd3033646029bdffd9ba3766068070
reniokta99 <reni.18411061@student.ubl.ac.id>
2020-10-13 09:35:36 +0700

    Tugas3_18411061_Renioktaviani

---
+++ app/src/main/java/com/reni/myapplication/users/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.reni.myapplication.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.reni.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnRegis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+
+        btnRegis = (Button) findViewById(R.id.btnRegis);
+
+        btnRegis.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,  RegistrasiActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }));
+
+    }
+}
---
+++ app/src/main/java/com/reni/myapplication/users/RegistrasiActivity.java
@@ -0,0 +1,16 @@
+package com.reni.myapplication.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.reni.myapplication.R;
+
+public class RegistrasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+    }
+}
